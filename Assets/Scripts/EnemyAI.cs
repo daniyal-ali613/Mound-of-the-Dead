7f317ba153b64cc000ca539885654600467343d8
Syed Daniyal Ali <54919310+Dany-Gamer@users.noreply.github.com>
2022-09-04 14:52:13 +0500

    Initial Commit

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform target;
+    public Transform enemyGFX;
+    public Animator animator;
+    public float dist;
+
+    public float speed = 200f;
+    public float nextWayPointDistance = 3f;
+    // public List <Sprite> enemySprites = new List<Sprite>();
+
+    Path path;
+    int currentWayPoint = 0;
+    bool reachedEndOfPath = false;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, .5f);
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWayPoint = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        dist = Vector2.Distance(this.gameObject.transform.position, target.position);
+
+        if (path == null) return;
+
+        if (currentWayPoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+
+        else
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWayPoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWayPoint]);
+
+        if (distance < nextWayPointDistance)
+        {
+            currentWayPoint++;
+        }
+
+        if (force.x >= 0.01f)
+            animator.SetBool("Horizontal", true);
+
+
+        else if (force.x <= 0.01f)
+        {
+            animator.SetBool("Horizontal", false);
+        }
+
+        if(dist < 5)
+        {
+           // speed = 0;
+        }
+    }
+}
